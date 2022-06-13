dd70a0ff90d794ae0ea5ea66e0d6bf57501837f4
Jorge <asdgfu@hotmail.com>
2022-06-13 14:53:43 -0700

    redundancias xd bombardeare yucatan

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -19,19 +19,95 @@ class BotManController extends Controller
 
         $botman->hears('{message}', function ($botman, $message) {
 
-            if ($message == 'hi') {
+            //mayusculas
+            if ($message == 'Hi') {
+                $this->askName($botman);
+            } elseif ($message == 'Hello') {
+                $this->askName($botman);
+            } elseif ($message == 'Hii') {
+                $this->askName($botman);
+            } elseif ($message == 'Good morning') {
+                $this->askName($botman);
+            } elseif ($message == 'Good evening') {
+                $this->askName($botman);
+            } elseif ($message == 'Hey') {
+                $this->askName($botman);
+            } elseif ($message == 'Price') {
+                $this->price($botman);
+            } elseif ($message == 'Prices') {
+                $this->price($botman);
+            } elseif ($message == 'How much?') {
+                $this->price($botman);
+            } elseif ($message == 'Services') {
+                $this->services($botman);
+            } elseif ($message == 'Location') {
+                $this->location($botman);
+            } elseif ($message == 'Where at') {
+                $this->location($botman);
+            } elseif ($message == 'State') {
+                $this->location($botman);
+            } elseif ($message == 'Place') {
+                $this->location($botman);
+            } elseif ($message == 'Located') {
+                $this->location($botman);
+            } elseif ($message == 'Who are you?') {
+                $this->history($botman);
+            } elseif ($message == 'History') {
+                $this->history($botman);
+            } elseif ($message == 'Company') {
+                $this->history($botman);
+            } elseif ($message == 'Booking') {
+                $this->booking($botman);
+            } elseif ($message == 'Appointment') {
+                $this->booking($botman);
+            } elseif ($message == 'Apointment') {
+                $this->booking($botman);
+            }elseif ($message == 'Help') {
+                $botman->reply("Send price to ask for our prices. \nSend services to learn about our current services. \nSend location to learn about our location.\nSend who are you? to learn about us and our history. \nSend booking to learn how to book an appointment with us.");
+
+
+            // minusculas
+            }elseif ($message == 'hi') {
                 $this->askName($botman);
             } elseif ($message == 'hello') {
                 $this->askName($botman);
+            } elseif ($message == 'hii') {
+                $this->askName($botman);
+            } elseif ($message == 'good morning') {
+                $this->askName($botman);
+            } elseif ($message == 'good evening') {
+                $this->askName($botman);
+            } elseif ($message == 'hey') {
+                $this->askName($botman);
             } elseif ($message == 'price') {
+                $this->price($botman);
+            } elseif ($message == 'prices') {
+                $this->price($botman);
+            } elseif ($message == 'how much?') {
                 $this->price($botman);
             } elseif ($message == 'services') {
                 $this->services($botman);
             } elseif ($message == 'location') {
                 $this->location($botman);
+            } elseif ($message == 'where at') {
+                $this->location($botman);
+            } elseif ($message == 'state') {
+                $this->location($botman);
+            } elseif ($message == 'place') {
+                $this->location($botman);
+            } elseif ($message == 'located') {
+                $this->location($botman);
             } elseif ($message == 'who are you?') {
                 $this->history($botman);
+            } elseif ($message == 'history') {
+                $this->history($botman);
+            } elseif ($message == 'company') {
+                $this->history($botman);
             } elseif ($message == 'booking') {
+                $this->booking($botman);
+            } elseif ($message == 'appointment') {
+                $this->booking($botman);
+            } elseif ($message == 'apointment') {
                 $this->booking($botman);
             }elseif ($message == 'help') {
                 $botman->reply("Send price to ask for our prices. \nSend services to learn about our current services. \nSend location to learn about our location.\nSend who are you? to learn about us and our history. \nSend booking to learn how to book an appointment with us.");
